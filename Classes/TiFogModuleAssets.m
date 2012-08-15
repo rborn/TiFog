6ff258c1fec74a56bf4a8a50196d46f6631ca810
Dan Tamas <dan.tamas@gmail.com>
2012-08-15 23:23:08 +0200

    initial commit

---
+++ Classes/TiFogModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "TiFogModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation TiFogModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
---
+++ Classes/TiUIView+TiUIWindow+Blurred.h
@@ -0,0 +1,18 @@
+#import "TiBase.h"
+#import "TiUIView.h"
+#import "TiUIWindow.h"
+
+@interface TiUIView (ViewWithShadow)
+
+
+-(void)setBlurred_:(id)value;
+
+@end
+
+
+@interface TiUIWindow (WindowWithShadow)
+
+
+-(void)setBlurred_:(id)value;
+
+@end
